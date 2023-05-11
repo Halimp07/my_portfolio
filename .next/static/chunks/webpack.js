ee80d926d06151196b2097ecd25be4cd872ab218
Halimp07 <halimputra0701@gmail.com>
2023-05-11 10:06:57 +0700

    update package.json, node modules

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "521c551753ba664a"; }
+/******/ 		__webpack_require__.h = function() { return "42e5acdfd2b99d5d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.dd7e8389a259e701.hot-update.js
+++ .next/static/webpack/webpack.dd7e8389a259e701.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2a218cbc82fa2b43"; }
+/******/ 	__webpack_require__.h = function() { return "b816901f665ca515"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
